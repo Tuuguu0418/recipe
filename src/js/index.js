d3ffead320d6d37d8a770e48d37f63fca46e9d67
Tuguldur <tuugii24758@gmail.com>
2024-08-04 12:59:27 +0800

    Buren duuslaa

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,16 @@
 import Search from "./model/Search";
 import { elements, renderLoader, clearLoader } from "./view/base";
 import * as searchView from "./view/searchView";
+import Recipe from "./model/Recipe";
+import List from "./model/List";
+import * as listView from "./view/listView";
+import Like from "./model/Like";
+import * as likesView from "./view/likesView";
+import {
+  renderRecipe,
+  clearRecipe,
+  highlightSelectedRecipe,
+} from "./view/recipeView";
 
 // Web app tolow
 // Hailtin query, ur dun
@@ -33,4 +43,115 @@ const controlSearch = async () => {
 elements.searchForm.addEventListener("submit", (e) => {
   e.preventDefault();
   controlSearch();
+});
+
+elements.pageButtons.addEventListener("click", (e) => {
+  const btn = e.target.closest(".btn-inline");
+
+  if (btn) {
+    const gotoPageNumber = parseInt(btn.dataset.goto, 10);
+    console.log(gotoPageNumber);
+    searchView.clearSearchResult();
+    searchView.renderRecipes(state.search.result, gotoPageNumber);
+  }
+});
+
+// Jorin controller
+const controlRecipe = async () => {
+  // 1. URL aas id g salgah
+  const id = window.location.hash.replace("#", "");
+
+  // URl dr id bga esehig shalgana
+  if (id) {
+    // 2. Jorin model iig uusgej ogno
+    state.recipe = new Recipe(id);
+    // 3. UI delgetsig beltgene
+    clearRecipe();
+    renderLoader(elements.recipeDiv);
+    highlightSelectedRecipe(id);
+    // 4. Joroo tataj awcrana
+    await state.recipe.getRecipe();
+    // 5. Jorig guitsetgeh hugatsaa bolon ortsig toostsoolno
+    clearLoader();
+    state.recipe.calcTime();
+    state.recipe.calcHuniiToo();
+    // 6. Joroo delgetsend uzuulne
+    renderRecipe(state.recipe, state.likes.isLiked(id));
+  }
+};
+
+// window.addEventListener("hashchange", controlRecipe);
+// window.addEventListener("load", controlRecipe);
+
+["hashchange", "load"].forEach((e) =>
+  window.addEventListener(e, controlRecipe)
+);
+
+window.addEventListener("load", (e) => {
+  //Shiner like model iig app ehelhed uusgene.
+  if (!state.likes) state.likes = new Like();
+
+  likesView.toggleLikeMenu(state.likes.getNumberOfLikes());
+
+  // Like uud bwl tsesig nemj haruulna
+  state.likes.likes.forEach((like) => likesView.renderLike(like));
+});
+// Nairlagnii controller
+
+const controlList = () => {
+  // Nairlagni model iig uusgene.
+  state.list = new List();
+  listView.clearItems();
+  state.recipe.ingredients.forEach((n) => {
+    const item = state.list.addItem(n);
+    // Tuhain nairlagig delgetsend haruulna
+    listView.renderItem(item);
+  });
+};
+
+const controlLike = () => {
+  // 1. Like iin model iig uusgeh.
+  if (!state.likes) state.likes = new Like();
+  // 2. Odoo haragdaj bga jorin id g awah
+  const currentRecipeId = state.recipe.id;
+  // 3. Ene jorig like hiisen esehig shalgah
+  if (state.likes.isLiked(currentRecipeId)) {
+    state.likes.deleteLike(currentRecipeId);
+    // Like iin tsesnees ustgana
+    likesView.deleteLike(currentRecipeId);
+
+    // Like towchnii like hiisn baidlig boliulah
+    likesView.toggleLikeBtn(false);
+  } else {
+    const newLike = state.likes.addLike(
+      currentRecipeId,
+      state.recipe.title,
+      state.recipe.publisher,
+      state.recipe.image_url
+    );
+
+    likesView.renderLike(newLike);
+    // Like towchnii like hiisn baidlig like hiisn bolgoh
+    likesView.toggleLikeBtn(true);
+  }
+
+  likesView.toggleLikeMenu(state.likes.getNumberOfLikes());
+};
+
+elements.recipeDiv.addEventListener("click", (e) => {
+  if (e.target.matches(".recipe__btn, .recipe__btn *")) {
+    controlList();
+  } else if (e.target.matches(".recipe__love, .recipe__love * ")) {
+    controlLike();
+  }
+});
+
+elements.shoppingList.addEventListener("click", (e) => {
+  // Click hiisen li elementin data-itemid attribute iig shuuj gargaj awna.
+  const id = e.target.closest(".shopping__item").dataset.itemid;
+
+  state.list.deleteItem(id);
+
+  // Delgetsees iim id tai ortsig ustgana ustgana.
+  listView.deleteItem(id);
 });
--- src/js/view/searchView.js
+++ src/js/view/searchView.js
@@ -1,9 +1,8 @@
 import { elements } from "./base";
 
 const renderRecipe = (recipe) => {
-  console.log(recipe);
   const markup = `<li>
-  <a class="results__link" href="${recipe.recipe_id}">
+  <a class="results__link" href=#${recipe.recipe_id}>
       <figure class="results__fig">
           <img src="${recipe.image_url}" alt="Test">
       </figure>
@@ -23,8 +22,42 @@ export const clearSearchQuery = () => {
 
 export const clearSearchResult = () => {
   elements.searchResultList.innerHTML = "";
+  elements.pageButtons.innerHTML = "";
 };
 export const getInput = () => elements.searchInput.value;
-export const renderRecipes = (recipes) => {
-  recipes.forEach(renderRecipe);
+
+export const renderRecipes = (recipes, currentPage = 1, resPerPage = 10) => {
+  // Hai;tin ur dung hudaslaj uzuuleh
+  const start = (currentPage - 1) * resPerPage;
+  const end = currentPage * resPerPage;
+
+  recipes.slice(start, end).forEach(renderRecipe);
+
+  const totaPages = Math.ceil(recipes.length / resPerPage);
+  renderButtons(currentPage, totaPages);
+};
+
+const createrButton = (
+  page,
+  type,
+  direction
+) => `<button class="btn-inline results__btn--${type}" data-goto=${page}>
+  <svg class="search__icon">
+      <use href="img/icons.svg#icon-triangle-${direction}"></use>
+  </svg>
+  <span>Хуудас ${page}</span>
+  </button>`;
+
+const renderButtons = (currentPage, totaPages) => {
+  let buttonHtml;
+  if (currentPage === 1 && totaPages > 1) {
+    buttonHtml = createrButton(2, "next", "right");
+  } else if (currentPage < totaPages) {
+    buttonHtml = createrButton(currentPage - 1, "prev", "left");
+    buttonHtml += createrButton(currentPage + 1, "next", "right");
+  } else if (currentPage === totaPages) {
+    buttonHtml = createrButton(currentPage - 1, "prev", "left");
+  }
+
+  elements.pageButtons.insertAdjacentHTML("afterbegin", buttonHtml);
 };
